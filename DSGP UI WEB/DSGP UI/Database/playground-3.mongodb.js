06cd11dc4011a5c72a4d841030efcbb776680811
Tharusha Liyanagama <tharushaliyanagama19@gmail.com>
2025-04-07 21:08:01 +0530

    Remove DSGP UI WEB folder from repository

--- DSGP UI WEB/DSGP UI/Database/playground-3.mongodb.js
+++
@@ -1,9 +0,0 @@
-use("User_Details");
-db.Username.insertMany([
-    {
-        "Username": "John",
-        "Password": "John123",
-        "Email": "john@gmail.com"}
-]);
-
-console.log(db.Username.find().pretty());
